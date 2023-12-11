a093b5c9448fabdd57167db0a2bbb7173c4e1afa
defox <wildmrdefox@gmail.com>
2023-12-11 20:35:45 +0700

    формат JSON

---
+++ JavaScript/Hard/06. Формат JSON/script.js
@@ -0,0 +1,40 @@
+// Вручную преобразуйте этот массив в строку в формате JSON.
+// let arr = [1, 2, 3, 'a', 'b', 'c'];
+// let json = '[1, 2, 3, "a", "b", "c"]';
+// let obj = `{
+// 	a: 1,
+// 	b: 2,
+// 	c: "eee",
+// 	d:true
+// }`;
+// let obj1 = `{
+// 	a: ["a", "b", "c"],
+// 	b: "111",
+// 	c: "eee"
+// }`;
+
+
+// Преобразуйте эту строку в настоящий массив JavaScript и найдите сумму чисел этого массива.
+// let json = '[1,2,3,4,5]';
+// let arr = JSON.parse(json)
+// let sum = 0;
+// for (let elem of arr) {
+//     sum += elem;
+// }
+// console.log(sum);
+
+
+// Преобразуйте этот массив в формат JSON.
+// let arr = ['user1', 'user2', 'user3', 'user4', 'user5'];
+// let json = JSON.stringify(arr);
+// console.log(json)
+
+
+// Добавьте в конец строки еще одного юзера.
+// let json = '["user1","user2","user3","user4","user5"]';
+
+// let arr = JSON.parse(json);
+// arr.push('user6');
+
+// let res = JSON.stringify(arr);
+// console.log(res);
