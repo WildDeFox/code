500e8fb85d5fa590f162b6d3ebaf0fc26d360bae
defox <wildmrdefox@gmail.com>
2024-01-24 20:56:50 +0700

    Работа с геттерами и сеттерами в ООП

--- PHP/ООП и MVC/01. Основы/06. Конструктор объекта в ООП/code.php
+++ PHP/ООП и MVC/01. Основы/06. Конструктор объекта в ООП/code.php
@@ -5,10 +5,18 @@
 // Создайте объект класса Employee с именем 'kyle', возрастом 30, зарплатой 2000.
 // Выведите на экран сумму зарплат созданных вами юзеров.
 
+// Сделайте класс Employee, в котором будут следующие приватные свойства: name, age и salary.
+// Сделайте геттеры и сеттеры для всех свойств класса Employee.
+// Дополните класс Employee приватным методом isAgeCorrect, который будет проверять
+// возраст на корректность (от 1 до 100 лет). Этот метод должен использоваться в сеттере setAge
+// перед установкой нового возраста (если возраст не корректный - он не должен меняться).
+// Пусть зарплата наших работников хранится в долларах.
+// Сделайте так, чтобы геттер getSalary добавлял в конец числа с зарплатой значок доллара
 class Employee {
-    public $name;
-    public $age;
-    public $salary;
+
+    private $name;
+    private $age;
+    private $salary;
 
     public function __construct($name, $age, $salary)
     {
@@ -16,9 +24,43 @@ class Employee {
         $this->age = $age;
         $this->salary = $salary;
     }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getSalary() {
+        return $this->salary . '$';
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setAge($age) {
+        if($this->isAgeCorrect($age)) {
+            $this->age = $age;
+        }
+    }
+
+    public function setSalary($salary) {
+        $this->salary = $salary;
+    }
+
+    private function isAgeCorrect($age) {
+        if($age > 1 and $age < 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 $emp1 = new Employee('eric', 25, 1000);
 $emp2 = new Employee('kyle', 30, 2000);
 
-echo $emp1->salary + $emp2->salary;
+echo $emp1->getSalary();
